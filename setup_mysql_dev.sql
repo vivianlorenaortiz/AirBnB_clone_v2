c11f251bcea720ed1a107ceee4ea484df4170f37
vivianlorenaortiz <973@holbertonschool.com>
2019-12-17 18:19:17 +0100

    adding file Grant permissions MySQL

---
+++ setup_mysql_dev.sql
@@ -0,0 +1,6 @@
+-- script that prepares a MySQL server
+CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+GRANT ALL PRIVILEGES ON * . * TO 'hbnb_dev'@'localhost';
+GRANT SELECT ON `performance_schema`.* TO 'hbnb_dev'@'localhost';
+FLUSH PRIVILEGES;
