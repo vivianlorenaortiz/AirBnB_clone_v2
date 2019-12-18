507734f922e15783bab92c9f5fb1a8ca6f1039a0
vivianlorenaortiz <973@holbertonschool.com>
2019-12-18 18:17:40 +0100

    review of the code

--- models/user.py
+++ models/user.py
@@ -2,6 +2,7 @@
 """This is the user class"""
 
 from models.base_model import BaseModel, Base
+import sqlalchemy
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 from os import getenv
@@ -15,8 +16,21 @@ class User(BaseModel, Base):
         first_name: first name
         last_name: last name
     """
-   __tablename__ = 'users'
+if models.storage_t == 'db':
+    __tablename__ = 'users'
     email = Column(String(128), nullable=False)
     password = Column(String(128), nullable=False)
     first_name = Column(String(128))
     last_name = Column(String(128))
+    reviews = relationship('Review', backref='user', cascade='delete')
+    places = relationship('Place', backref='user', cascade='delete')
+
+else:
+    email = ""
+    password = ""
+    first_name = ""
+    last_name = ""
+
+    def __init__(self, *args, **kwargs):
+        """initializes user"""
+        super().__init__(*args, **kwargs)
