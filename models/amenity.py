dbd3bb8e1863f089c95397785401bcc2012cf806
German Franco F <933@holbertonschool.com>
2019-12-18 22:25:39 +0000

    [fix] remove attributes to make it compile

--- models/amenity.py
+++ models/amenity.py
@@ -9,7 +9,7 @@ from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 
 
-class Amenity(BaseModel):
+class Amenity(BaseModel, Base):
     """This is the class for Amenity
     Attributes:
         name: input name
@@ -17,5 +17,3 @@ class Amenity(BaseModel):
 
     __tablename__ = 'amenities'
     name = Column(String(128), nullable=False)
-    place_amenities = relationship("Place", secondary=place_amenity,
-                                   backref='amenities')
