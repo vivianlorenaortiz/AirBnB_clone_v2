825940c65bef453175bf579e70a0cf919994ae71
vivianlorenaortiz <973@holbertonschool.com>
2019-12-18 20:47:23 +0100

    adding file exercise nine

--- models/review.py
+++ models/review.py
@@ -10,6 +10,7 @@ class Review(BaseModel):
         user_id: user id
         text: review description
     """
-    place_id = ""
-    user_id = ""
-    text = ""
+    __tablename__ = "reviews"
+    place_id = Column(String(60), ForeignKey("places.id"), nullable=False)
+    user_id = Column(String(60), ForeignKey("users.id"), nullable=False)
+    text = Column(String(1024), nullable=False)
