5c5ab0798987d4e04c6342451aaa647c11d16281
vivianlorenaortiz <973@holbertonschool.com>
2019-12-18 17:43:04 +0100

    user.py

--- models/user.py
+++ models/user.py
@@ -1,6 +1,12 @@
 #!/usr/bin/python3
 """This is the user class"""
-from models.base_model import BaseModel
+
+from models.base_model import BaseModel, Base
+from models.place import Place
+from models.review import Review
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
+from os import getenv
 
 
 class User(BaseModel):
@@ -11,6 +17,14 @@ class User(BaseModel):
         first_name: first name
         last_name: last name
     """
+    __tablename__ = 'user'
+    email = Column(String(128), nullable=False)
+    password = Column(Strinf(128), nullable=False)
+    first_name = Column(String(128))
+    last_name = Column(String(128))
+    places = relationship("Place", backref="user", cascade="delete")
+    reviews = relationship("Review", backref="user", cascade="delete")
+else:
     email = ""
     password = ""
     first_name = ""
