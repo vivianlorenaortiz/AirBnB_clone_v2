6133ec457f0e0c3631017e8e573b76f82b002320
vivianlorenaortiz <973@holbertonschool.com>
2020-01-04 01:45:30 +0100

    setup web static

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -6,5 +6,5 @@ sudo mkdir -p /data/web_static/releases/test /data/web_static/shared
 echo "testing." | sudo tee /data/web_static/releases/test/index.html
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 sudo chown -hR ubuntu:ubuntu /data/
-sudo sed -i '\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
+sudo sed -i '38i\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
 sudo service nginx restart
