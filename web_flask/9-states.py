d57772bf2ea3fb26cf40980510e2051dbbdf817a
vivianlorenaortiz <973@holbertonschool.com>
2020-01-23 04:05:40 +0100

    4

---
+++ web_flask/9-states.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Write a script that starts a Flask web application"""
+from flask import Flask
+from flask import render_template
+from models import storage
+
+app = Flask(__name__)
+
+
+@app.teardown_appcontext
+def teardown_session(self):
+    storage.close()
+
+
+@app.route('/states')
+@app.route('/states/<id>')
+def show_state(id=None):
+    if id is not None:
+        id = 'State.'+id
+    return render_template(
+        '9-states.html', states=storage.all("State"), state_id=id)
