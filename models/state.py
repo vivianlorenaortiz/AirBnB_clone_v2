574506cce95f83e1b09a0be9e7a13fefc54a7911
vivianlorenaortiz <vivianlorena0414@gmail.com>
2020-01-24 13:53:59 -0500

    Update

--- models/state.py
+++ models/state.py
@@ -4,6 +4,7 @@ from models.base_model import BaseModel, Base
 from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.orm import relationship
 import os
+import models
 
 
 class State(BaseModel, Base):
