2702f70494e5f6b393557ffd86ad66f548f1b65a
vivianlorenaortiz <vivianlorena0414@gmail.com>
2020-01-24 13:49:55 -0500

    Update

--- models/state.py
+++ models/state.py
@@ -14,7 +14,7 @@ class State(BaseModel, Base):
     __tablename__ = 'states'
     name = Column(String(128))
     cities = relationship('City', cascade='all, delete', backref='state')
-    if os.getenv('HBNB_TYPE_STORAGE') == 'db':
+    if os.getenv('HBNB_TYPE_STORAGE') != 'db':
         @property
         def cities(self):
             all_objs_list = []
