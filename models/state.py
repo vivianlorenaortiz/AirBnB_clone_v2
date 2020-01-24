8d296ad58112c25aa9b8a65d0fdd5af2c5ce9421
vivianlorenaortiz <vivianlorena0414@gmail.com>
2020-01-24 13:58:43 -0500

    Update

--- models/state.py
+++ models/state.py
@@ -1,26 +1,29 @@
 #!/usr/bin/python3
 """This is the state class"""
-from models.base_model import BaseModel, Base
-from sqlalchemy import Column, Integer, String, ForeignKey
-from sqlalchemy.orm import relationship
-import os
-from models import storage
+from models.base_model import Base, BaseModel
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship, backref
+from os import environ
+import models
 
 class State(BaseModel, Base):
     """This is the class for State
     Attributes:
         name: input name
     """
-    __tablename__ = 'states'
-    name = Column(String(128))
-    cities = relationship('City', cascade='all, delete', backref='state')
-    if os.getenv('HBNB_TYPE_STORAGE') != 'db':
+    __tablename__ = "states"
+    name = Column(String(128),
+                  nullable=False)
+    cities = relationship("City",
+                          backref="states",
+                          cascade="all, delete-orphan")
+    if environ.get('HBNB_TYPE_STORAGE') != 'db':
         @property
         def cities(self):
-            all_objs_list = []
-            all_objs = storage.all(City)
+            """ getter attribute that connects relationship"""
+            cities = [
+                value for key, value in models.storage.all(models.City).items()
+                if value.state_id == self.id
+            ]
+            return cities
 
-            for city in all_objs.values():
-                if city.state_id == self.id:
-                    all_objs_list.append(city)
-            return all_objs_list
